58a2fe48d727df676c690eff32cbab39659419a4
Tooru Wu <tom123463@icloud.com>
2025-09-19 14:03:12 +0800

    feat: set tailwind enviroment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-
 function App() {
-
   return (
-    <>
-    
-    </>
-  )
+    <div className="bg-gray-500 w-full h-full min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold underline">This is Tailwind Test</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
