de1268b8ee4658a8665ea1550d79f00e08caeb75
Tooru Wu <tom123463@icloud.com>
2025-09-22 16:24:17 +0800

    feat: feat react hook form

--- src/components/views/Home/LoginForm/index.tsx
+++ src/components/views/Home/LoginForm/index.tsx
@@ -1,5 +1,20 @@
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  return (
+    <div>
+      <h2>登入</h2>
+      <div>
+        <p>請輸入帳號密碼</p>
+        <p>如需新增帳號，請聯絡管理員</p>
+      </div>
+      <div>
+        <label>帳號</label>
+        <input type="text" />
+        <label>密碼</label>
+        <input type="password" />
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default LoginForm;
