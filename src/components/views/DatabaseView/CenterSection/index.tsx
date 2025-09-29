8c6e0e40b8538f45f6c1cc2d3214de7a775f2f4d
Tooru Wu <tom123463@icloud.com>
2025-09-29 20:23:58 +0800

    update(CerterSection): change h2 title with url params

--- src/components/views/DatabaseView/CenterSection/index.tsx
+++ src/components/views/DatabaseView/CenterSection/index.tsx
@@ -1,10 +1,13 @@
 import ExampleTable from "../ExampleTable";
+import { useParams } from "react-router-dom";
+import { TABS_CONSTANTS } from "../Tabs/constants";
 
 const CenterSection = () => {
+  const { tab } = useParams();
   return (
     <div className="rounded-xl border border-primary/10 shadow-md w-full p-6">
       <div className=" flex justify-between items-center">
-        <h2 className="text-h2 font-normal">Table Name</h2>{" "}
+        <h2 className="text-h2 font-normal">{TABS_CONSTANTS.find((item) => item.id === tab)?.label}</h2>
         <div>
           <input type="Search" />
         </div>
