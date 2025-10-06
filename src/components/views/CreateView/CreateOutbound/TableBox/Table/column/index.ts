16843c62b58e078a0c083527169465905988e508
Tooru Wu <tom123463@icloud.com>
2025-10-06 17:44:29 +0800

    feat(CreateOutBound): create fakedata and columns.

--- src/components/views/CreateView/CreateOutbound/TableBox/Table/column/index.ts
+++ src/components/views/CreateView/CreateOutbound/TableBox/Table/column/index.ts
@@ -1,6 +1,62 @@
 import { createColumnHelper } from "@tanstack/react-table";
 import { CreateOutbound } from "../type";
+import StringCell from "../Cell/SelecterCell";
+import NumberCell from "../Cell/NumberCell";
 
 const columnHelper = createColumnHelper<CreateOutbound>();
 
 const columns = [
+  columnHelper.accessor("customerName", {
+    cell: StringCell,
+    header: "客戶名",
+    enableColumnFilter: true,
+  }),
+  columnHelper.accessor("productName", {
+    cell: StringCell,
+    header: "商品名",
+    enableColumnFilter: true,
+  }),
+  columnHelper.accessor("unit", {
+    cell: StringCell,
+    header: "單位",
+    enableColumnFilter: false,
+  }),
+  columnHelper.accessor("costPerUnit", {
+    cell: NumberCell,
+    header: "單位成本",
+    enableColumnFilter: false,
+  }),
+
+  columnHelper.accessor("pricePerUnit", {
+    cell: NumberCell,
+    header: "單位價格",
+    enableColumnFilter: false,
+  }),
+  columnHelper.accessor("quantity", {
+    cell: NumberCell,
+    header: "數量",
+    enableColumnFilter: false,
+  }),
+  columnHelper.accessor("totalPrice", {
+    cell: NumberCell,
+    header: "小計",
+    enableColumnFilter: false,
+  }),
+  columnHelper.accessor("netProfit", {
+    cell: NumberCell,
+    header: "損益",
+    enableColumnFilter: false,
+  }),
+  columnHelper.accessor("remark", {
+    cell: StringCell,
+    header: "備註",
+    enableColumnFilter: true,
+  }),
+  columnHelper.accessor("shipmentDate", {
+    cell: StringCell,
+    header: "出貨日期",
+    enableColumnFilter: true,
+  }),
+];
+
+export { columns };
--- src/components/views/CreateView/CreateOutbound/TableBox/Table/index.tsx
+++ src/components/views/CreateView/CreateOutbound/TableBox/Table/index.tsx
@@ -1,13 +1,48 @@
-import { useReactTable, getCoreRowModel } from "@tanstack/react-table";
-import { useState } from "react";
-
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+} from "@tanstack/react-table";
+import { columns } from "./column";
+import data from "./data/inedx";
 const Table = () => {
-    const [data,setData] = useState([])
-    const table = useReactTable({
-        data,columns,getCoreRowModel:getCoreRowModel()
-    })
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
   return (
-      <h1>Table</h1>
+    <div className="w-full h-220 px-4 overflow-y-auto">
+      <table className="w-full ">
+        <thead className="border-b-2 border-primary/10 h-14 sticky top-0 bg-white z-10">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id} className="">
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
