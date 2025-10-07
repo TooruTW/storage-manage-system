b4b0a5ef793ea8625425561262b7f9289055d6d0
Tooru Wu <tom123463@icloud.com>
2025-10-07 14:15:42 +0800

    feat(SearchCustom): refactor form logic, use controller to watch customername.

--- src/components/views/CreateView/CreateOutbound/CreatingBox/SearchCustom/index.tsx
+++ src/components/views/CreateView/CreateOutbound/CreatingBox/SearchCustom/index.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from "react";
-import { FAKE_CUSTOM_LIST } from "./constants/FAKE_CUSTOM_LIST";
-import { Control } from "react-hook-form";
+import { Control, Controller } from "react-hook-form";
 import { CreateOutbound } from "../../type";
+import SearchInput from "./SearchInput";
 
 type SearchCustomProps = {
   setIsAddNewCustom: (isAddNewCustom: boolean) => void;
@@ -9,15 +8,6 @@ type SearchCustomProps = {
 };
 
 const SearchCustom = ({ setIsAddNewCustom, control }: SearchCustomProps) => {
-  const [customList, setCustomList] = useState<string[]>([]);
-  const [keyword, setKeyword] = useState<string>("");
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  useEffect(() => {
-    setCustomList(
-      FAKE_CUSTOM_LIST.filter((custom) => custom.includes(keyword))
-    );
-  }, [keyword]);
-
   return (
     <div className="">
       <p
@@ -28,33 +18,16 @@ const SearchCustom = ({ setIsAddNewCustom, control }: SearchCustomProps) => {
       </p>
       <div className="flex gap-2 w-full relative">
         <h2 className="text-h2">客戶：</h2>
-        <input
-          type="text"
-          className="border-b-1 border-primary/10 py-1 px-2 shadow-xs w-full"
-          placeholder="請輸入客戶名稱"
-          value={keyword}
-          onFocus={() => setIsOpen(true)}
-          onChange={(e) => {
-            setKeyword(e.target.value);
+        <Controller
+          name="customerName"
+          control={control}
+          rules={{
+            required: "請選擇客戶",
           }}
+          render={({ field }) => (
+            <SearchInput value={field.value || ""} onChange={field.onChange} />
+          )}
         />
-        {isOpen && (
-          <div className="w-full h-fit max-h-200 overflow-y-auto absolute left-0 top-full translate-y-2 divide-y-1 divide-primary/10 border-1 border-primary rounded-md bg-white z-10 text-center">
-            {customList.map((custom) => (
-              <p
-                key={custom}
-                onClick={() => {
-                  setKeyword(custom);
-                  control._formValues.customerName = custom;
-                  setIsOpen(false);
-                }}
-                className="text-paragraph py-2 hover:bg-primary/10 cursor-pointer"
-              >
-                {custom}
-              </p>
-            ))}
-          </div>
-        )}
       </div>
     </div>
   );
