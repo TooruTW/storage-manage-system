b2113c10a2f839f2a1ad575ebe83b5db56475fe2
Tooru Wu <tom123463@icloud.com>
2025-10-14 16:15:46 +0800

    update: refactor getConsignmentApi logic to destructure data from supabase.

--- src/api/supabase/consignmentApi/useGetConsignmentApi.ts
+++ src/api/supabase/consignmentApi/useGetConsignmentApi.ts
@@ -1,18 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
 import supabase from "..";
 
+
 const getConsignmentApi = async () => {
-  const { data: consignment, error } = await supabase.from("consignment").select("*");
+  const { data: consignment, error } = await supabase
+    .from("consignment")
+    .select(
+      "id, product_name, unit, current_stock, last_update_date, customer:customer_id(name)"
+    );
 
-
-  if (error) {
+  if (error || !consignment) {
     console.error("Get consignment error", error);
     throw error;
   }
 
   console.log("data", consignment);
 
-  return consignment;
+  const joinedData = consignment.map((item: any) => {
+    return {
+      id: item.id,
+      product_name: item.product_name,
+      unit: item.unit,
+      current_stock: item.current_stock,
+      last_update_date: item.last_update_date,
+      name: item.customer.name,
+    };
+  });
+
+  return joinedData;
 };
 
 const useGetConsignmentApi = () => {
