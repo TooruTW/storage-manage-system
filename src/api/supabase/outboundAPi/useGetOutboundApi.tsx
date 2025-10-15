bc4ea9680146199214fd0b75621a0694c1528226
Tooru Wu <tom123463@icloud.com>
2025-10-15 15:36:36 +0800

    refactor: update getOutboundApi to include inbound product details and improve data structure in response

--- src/api/supabase/outboundAPi/useGetOutboundApi.tsx
+++ src/api/supabase/outboundAPi/useGetOutboundApi.tsx
@@ -5,27 +5,28 @@ const getOutboundApi = async () => {
   const { data: outbound, error } = await supabase
     .from("outbound")
     .select(
-      "id, customer:customer_id(name), product_name, unit, cost_per_unit, quantity, price_per_unit, shipment_date, total_price, net_profit, remark"
+      "id, customer:customer_id(name), inbound:product_id(product_name,unit), cost_per_unit, quantity, price_per_unit, shipment_date, total_price, net_profit, remark"
     );
   if (error) {
     console.error("Get outbound error", error);
     throw error;
   }
 
+  console.log("outbound", outbound);
 
   const joinedData = outbound.map((item: any) => {
     return {
-     id: item.id,
-     name: item.customer.name,
-     product_name: item.product_name,
-     unit: item.unit,
-     cost_per_unit: item.cost_per_unit,
-     quantity: item.quantity,
-     price_per_unit: item.price_per_unit,
-     shipment_date: item.shipment_date,
-     total_price: item.total_price,
-     net_profit: item.net_profit,
-     remark: item.remark || "",
+      id: item.id,
+      name: item.customer.name,
+      product_name: item.inbound.product_name,
+      unit: item.inbound.unit || "",
+      cost_per_unit: item.cost_per_unit,
+      quantity: item.quantity,
+      price_per_unit: item.price_per_unit,
+      shipment_date: item.shipment_date,
+      total_price: item.total_price,
+      net_profit: item.net_profit,
+      remark: item.remark || "",
     };
   });
 
