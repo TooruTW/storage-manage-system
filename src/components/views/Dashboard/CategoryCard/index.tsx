c4a3a78c4f5441a5b024008c9515755c8f6a8c2a
Tooru Wu <tom123463@icloud.com>
2025-10-21 20:39:34 +0800

    refactor(CategoryCard):  resturcture code for better readability.

--- src/components/views/Dashboard/CategoryCard/index.tsx
+++ src/components/views/Dashboard/CategoryCard/index.tsx
@@ -1,12 +1,12 @@
-import { DollarSign } from "lucide-react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { DollarSign } from "lucide-react";
 import { CategoryProps } from "./type";
+import { useCallback } from "react";
 
 const FakeCategoryList: CategoryProps[] = [
   { id: "purchaseAmount", label: "進貨額", value: 100 },
   { id: "salesAmount", label: "營業額", value: 200 },
 ];
-
 
 const CategoryCard = () => {
   const { category } = useParams();
@@ -21,16 +21,22 @@ const CategoryCard = () => {
     navigate(newPath);
   };
 
+  const activeStyle = useCallback(
+    (id: CategoryProps["id"]) => {
+      const style = "bg-primary/20";
+      const inactiveStyle = "border-2 border-primary/10";
+      return category === id ? style : inactiveStyle;
+    },
+    [category]
+  );
+
   return (
     <div className="w-full flex gap-7">
       {FakeCategoryList.map((item) => (
         <div
           key={item.id}
-          className={`w-full rounded-md p-6 shadow-xs cursor-pointer ${
-            category === item.id
-              ? "bg-primary/20"
-              : "border-2 border-primary/10"
-          }`}
+          className={`w-full rounded-md p-6 shadow-xs cursor-pointer 
+            ${activeStyle(item.id)}`}
           onClick={() => switchCategory(item.id)}
         >
           <div>
