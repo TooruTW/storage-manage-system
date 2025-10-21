e28775072ff68c516e8254489be90016d60b0055
Tooru Wu <tom123463@icloud.com>
2025-10-21 14:10:29 +0800

    refactor(Home): optimize styling logic and state management for login view

--- src/components/views/Home/index.tsx
+++ src/components/views/Home/index.tsx
@@ -2,17 +2,25 @@ import "dayjs/locale/zh-tw";
 import MainImage from "./MainImage";
 import { Outlet, useLocation } from "react-router-dom";
 import TimeComponent from "./TimeComponent";
+import { useEffect, useMemo, useState } from "react";
 
 const Home = () => {
   const location = useLocation();
-  const isLoggingIn = location.pathname === "/home/login";
+  const isLoggingIn = useMemo(
+    () => location.pathname === "/home/login",
+    [location.pathname]
+  );
 
+  const [style, setStyle] = useState<string>("gap-7");
+
+  useEffect(() => {
+    if (isLoggingIn) setStyle("gap-1");
+    else setStyle("gap-7");
+  }, [isLoggingIn]);
 
   return (
     <div
-      className={`w-full h-screen flex flex-col items-center justify-center py-4 ${
-        !isLoggingIn ? "gap-7" : "gap-1"
-      }`}
+      className={`w-full h-screen flex flex-col items-center justify-center py-4 ${style}`}
     >
       <h1 className="text-h1 font-bold">進出貨管理系統</h1>
       <MainImage />
