0b264400395005d8833d0a9102563746c95ece3d
Tooru Wu <tom123463@icloud.com>
2025-10-24 18:20:36 +0800

    refactor(ButtonContainer): simplify button list definitions and remove unused onClick property from ButtonList type

--- src/components/views/Home/ButtonContainer/constants.ts
+++ src/components/views/Home/ButtonContainer/constants.ts
@@ -1,38 +1,19 @@
-
-
-const unLoginButtonList = [
-  {
-    label: "登入",
-    path: "/home/login",
-  },
-];
+const unLoginButtonList = [{ label: "登入", path: "/home/login" }];
 
 const loginButtonList = [
-  {
-    label: "儀表板",
-    path: "/dashboard/month/purchaseAmount",
-  },
-  {
-    label: "資料庫",
-    path: "/database",
-  },
-  {
-    label: "新增",
-    path: "/create",
-  },
-  {
-    label: "登出",
-    path: "/logout",
-  },
+  { label: "儀表板", path: "/dashboard/month/purchaseAmount" },
+  { label: "資料庫", path: "/database" },
+  { label: "新增", path: "/create" },
+  { label: "登出", path: "/logout" },
 ];
 
 const mobileLoginButtonList = [
-  {label: "查價錢", path: "/mobile-database/price"},
-  {label: "查成本", path: "/mobile-database/cost"},
-  {label: "查客戶", path: "/mobile-database/customer"},
-  {label: "查廠商", path: "/mobile-database/supplier"},
-  {label: "查庫存", path: "/mobile-database/inventory"},
-  {label: "登出", path: "/logout"},
+  { label: "查價錢", path: "/mobile-database/price" },
+  { label: "查成本", path: "/mobile-database/cost" },
+  { label: "查客戶", path: "/mobile-database/customer" },
+  { label: "查廠商", path: "/mobile-database/supplier" },
+  { label: "查庫存", path: "/mobile-database/inventory" },
+  { label: "登出", path: "/logout" },
 ];
 
 export { unLoginButtonList, loginButtonList, mobileLoginButtonList };
--- src/components/views/Home/ButtonContainer/type.ts
+++ src/components/views/Home/ButtonContainer/type.ts
@@ -1,7 +1,6 @@
 type ButtonList = {
     label: string;
     path: string;
-    onClick?: () => void;
   };
 
 export type { ButtonList };
