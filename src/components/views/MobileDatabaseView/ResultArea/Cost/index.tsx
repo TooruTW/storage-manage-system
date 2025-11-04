9654636d68be13fd0698fe79c7cba934560a8bd0
Tooru Wu <tom123463@icloud.com>
2025-11-04 22:02:56 +0800

    feat(Cost): implement row virtualization for enhanced rendering performance and update item display structure

--- src/components/views/MobileDatabaseView/ResultArea/Cost/index.tsx
+++ src/components/views/MobileDatabaseView/ResultArea/Cost/index.tsx
@@ -4,7 +4,7 @@ import { TableStateView } from "@/components/views/DatabaseView/shared";
 import ConditionRequired from "../shared/ConditionRequired";
 
 import { InboundType } from "@/types/InboundType";
-
+import { useWindowVirtualizer } from "@tanstack/react-virtual";
 
 const Cost = ({ object, product }: { object: string; product: string }) => {
   const [data, setData] = useState<InboundType[]>([]);
@@ -17,17 +17,20 @@ const Cost = ({ object, product }: { object: string; product: string }) => {
     if (!inboundData || isLoading) return;
     let tempData = [...inboundData];
     if (object !== "") {
-      tempData = tempData.filter((item) =>
-        item.supplier_name.includes(object)
-      );
+      tempData = tempData.filter((item) => item.supplier_name.includes(object));
     }
     if (product !== "") {
-      tempData = tempData.filter((item) =>
-        item.product_name.includes(product)
-      );
+      tempData = tempData.filter((item) => item.product_name.includes(product));
     }
     setData(tempData);
   }, [object, product, inboundData, isLoading, isFiltering]);
+
+  const rowVirtualizer = useWindowVirtualizer({
+    count: data.length,
+    estimateSize: () => 90,
+    overscan: 5,
+  });
+  const virtualItems = rowVirtualizer.getVirtualItems();
 
   if (isLoading) return <TableStateView type="loading" />;
   if (!data) return <TableStateView type="empty" />;
@@ -36,40 +39,67 @@ const Cost = ({ object, product }: { object: string; product: string }) => {
     return <ConditionRequired />;
   }
   return (
-    <ul className="w-full h-full overflow-y-auto flex flex-col gap-2 pb-20">
-      {data.map(
-        (item, index) => {
-          return (
-            <li key={`${item.id}-${index}-${item.inbound_date}`} className="w-full flex flex-col">
-              <div className="w-fit text-balance rounded-t-md bg-primary text-primary-foreground px-2">
-                <p>{item.supplier_name}</p>
-              </div>
-              <div className="w-full flex gap-4 rounded-b-md rounded-tr-md bg-primary/10 p-2">
-                <div className="flex flex-col w-1/3">
-                  <div>{item.product_name}</div>
-                  <div className="text-label text-primary/50">
-                    單位：<span>{item.unit}</span>
+    <ul
+      className="w-full flex flex-col gap-2"
+      style={{
+        height: `${rowVirtualizer.getTotalSize()}px`,
+        position: "relative",
+      }}
+    >
+      {" "}
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          transform: `translateY(${virtualItems[0]?.start ?? 0}px)`,
+        }}
+      >
+        {virtualItems.length > 0 &&
+          virtualItems.map((virtualItem) => {
+            const {
+              id,
+              inbound_date,
+              supplier_name,
+              product_name,
+              unit,
+              quantity,
+              price_per_unit,
+            } = data[virtualItem.index];
+            return (
+              <li
+                key={`${id}-${virtualItem.index}-${inbound_date}`}
+                className="w-full flex flex-col mb-2"
+              >
+                <div className="w-fit text-balance rounded-t-md bg-primary text-primary-foreground px-2">
+                  <p>{supplier_name}</p>
+                </div>
+                <div className="w-full flex gap-4 rounded-b-md rounded-tr-md bg-primary/10 p-2">
+                  <div className="flex flex-col w-1/3">
+                    <div>{product_name}</div>
+                    <div className="text-label text-primary/50">
+                      單位：<span>{unit}</span>
+                    </div>
+                  </div>
+                  <div className="flex justify-between flex-1">
+                    <div className="flex flex-col">
+                      <span className="text-label text-primary/50">數量:</span>
+                      <span className="self-end">{quantity}</span>
+                    </div>
+                    <div className="flex flex-col">
+                      <span className="text-label text-primary/50">成本:</span>
+                      <span className="self-end">$ {price_per_unit}</span>
+                    </div>
+                    <div className="flex justify-end items-center">
+                      <p className="text-label">{inbound_date}</p>
+                    </div>
                   </div>
                 </div>
-                <div className="flex justify-between flex-1">
-                  <div className="flex flex-col">
-                    <span className="text-label text-primary/50">數量:</span>
-                    <span className="self-end">{item.quantity}</span>
-                  </div>
-                  <div className="flex flex-col">
-                    <span className="text-label text-primary/50">成本:</span>
-                    <span className="self-end">$ {item.price_per_unit}</span>
-                  </div>
-                  <div className="flex justify-end items-center">
-                    <p className="text-label">{item.inbound_date}</p>
-                  </div>
-                </div>
-              </div>
-            </li>
-          );
-        },
-        [data]
-      )}
+              </li>
+            );
+          })}
+      </div>
     </ul>
   );
 };
