38f58279851b2150aba4fe1830c3b10a460d295e
Tooru Wu <tom123463@icloud.com>
2025-11-05 15:28:50 +0800

    refactor(usePostCustomerApi): update supplier to customer

--- src/api/supabase/customerApi/usePostCustomerApi.ts
+++ src/api/supabase/customerApi/usePostCustomerApi.ts
@@ -1,18 +1,18 @@
 import supabase from "..";
-import { SupplierType } from "@/types/SupplierType";
+import { CustomerType } from "@/types/CustomerType";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const postCustomerApi = async (supplier: Partial<SupplierType>) => {
+const postCustomerApi = async (customer: Partial<CustomerType>) => {
   const { data, error } = await supabase
     .from("customer")
-    .insert([supplier])
+    .insert([customer])
     .select();
 
   if (error) {
-    console.error("Post supplier error", error);
+    console.error("Post customer error", error);
     throw error;
   }
-  return data as unknown as SupplierType[];
+  return data as unknown as CustomerType[];
 };
 
 const usePostCustomerApi = () => {
